238dd4c9b19faf485530f01b5a2eec4bf58cb165
razdobudko.s <razdobudko.s@wb.ru>
2024-04-20 19:30:29 +0200

    Composition Lifecycle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         google()
@@ -15,4 +17,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AndrushchenkoJetpackComposeCourse"
 include(":recycler-view-example")
- 
+include(":simple-counter")
