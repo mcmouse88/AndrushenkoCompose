d8aa19f84a856c6263be9a5ee3982abfca87709d
razdobudko.s <razdobudko.s@wb.ru>
2024-04-26 00:01:46 +0200

    Image & Icon & Coil - Show Images in Jetpack Compose

--- simple-counter/src/main/java/com/mcmouse88/simple_counter/base/RowContainer.kt
+++ simple-counter/src/main/java/com/mcmouse88/simple_counter/base/RowContainer.kt
@@ -1,12 +1,15 @@
 package com.mcmouse88.simple_counter.base
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -16,13 +19,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun Container(
+fun RowContainer(
     name: String,
-    content: @Composable ColumnScope.() -> Unit
+    content: @Composable BoxScope.() -> Unit
 ) {
     Card(
         border = BorderStroke(width = 1.dp, Color.Gray),
@@ -30,21 +34,31 @@ fun Container(
         shape = RoundedCornerShape(size = 4.dp),
         modifier = Modifier
             .fillMaxWidth(0.8f)
-            .padding(top = 12.dp)
+            .heightIn(min = 0.dp, max = 100.dp)
+            .padding(vertical = 6.dp, horizontal = 12.dp)
     ) {
-        Column(
+        Row(
             modifier = Modifier
                 .padding(8.dp)
                 .fillMaxWidth()
         ) {
+            Box(
+                content = content,
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.weight(1.0f)
+            )
+            Spacer(modifier = Modifier.width(6.dp))
             Text(
                 text = name,
                 fontSize = 14.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(2.0f)
             )
             Spacer(modifier = Modifier.height(8.dp))
-            content.invoke(this)
         }
     }
 }
